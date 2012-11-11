0272febb02ba064f7231886d8cd3eb679cda1841
Juanan Lebrijo <jlebrijo@gmail.com>
2012-11-11 01:59:28 +0100

    #151: Refactoring Omniauth access

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,16 +1,13 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  before_filter :get_auth
   def facebook
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    @user = User.find_for_oauth(@auth, @auth.extra.raw_info.name, @auth.info.email)
+
+    redirect_to_success_or_registration_again
   end
   def twitter
-    @user = User.find_for_twitter_oauth(env['omniauth.auth'])
+    @user = User.find_for_oauth(@auth, @auth.info.name, "fake@fake.com")
+    # Ask for mail if is a new user
     if params["email"] == nil && @user.email =="fake@fake.com"
       render "devise/registrations/write_mail"
     else
@@ -18,12 +15,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         @user.email = params["email"] 
         @user.save
       end
-      if @user.persisted?
-        sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-        set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
-      else
-        redirect_to new_user_registration_url
-      end
+      redirect_to_success_or_registration_again
+    end
+  end
+  private
+  def get_auth
+    @auth = request.env["omniauth.auth"]
+  end
+  def redirect_to_success_or_registration_again
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => @auth.provider.capitalize) if is_navigational_format?
+    else
+      redirect_to new_user_registration_url
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,37 +13,17 @@ class User < ActiveRecord::Base
 
   # Omniauth
   attr_accessible :provider, :uid
-  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+  def self.find_for_oauth(auth, name, email)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     unless user
-      user = User.create(name:auth.extra.raw_info.name,
-                           provider:auth.provider,
+      user = User.create(provider:auth.provider,
                            uid:auth.uid,
-                           email:auth.info.email,
+                           name: name,
+                           email: email,
                            password:Devise.friendly_token[0,20]
                            )
     end
     user
-  end
-  def self.find_for_twitter_oauth(auth)
-    user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    unless user
-      user = User.create(name:auth.info.name,
-                           provider:auth.provider,
-                           uid:auth.uid,
-                           email: "fake@fake.com",
-                           password:Devise.friendly_token[0,20]
-                           )
-    end
-    user
-  end
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      # Gets facebook mail if exists
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
   end
   
   # Relations with model
