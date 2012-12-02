f4e96e9253a2124c1fa12575d4fde5da2cde93d1
Juanan Lebrijo <jlebrijo@gmail.com>
2012-12-02 23:57:08 +0100

    #155: Eliminating Admin user from old devise role creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'CREATING ROLES'
 Role.create([
-  { :name => 'admin' }, 
   { :name => 'organizer' }, 
   { :name => 'atendee' }
 ], :without_protection => true)
 puts 'SETTING UP DEFAULT USER LOGIN'
-admin = User.create! :name => 'Admin User', :email => 'admin@gogetix.com', :password => 'please', :password_confirmation => 'please'
-puts 'New user created: ' << admin.name
 organizer = User.create! :name => 'Organizer User', :email => 'organizer@gogetix.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << organizer.name
 attendee = User.create! :name => 'Attendee User', :email => 'attendee@gogetix.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << attendee.name
-admin.add_role :admin
 organizer.add_role :organizer
 attendee.add_role :attendee
