07aa5fbc79dca15a2ce45ca49e988becd880428a
Juan Lebrijo <juan@lebrijo.com>
2012-12-10 14:23:59 +0100

    Pink buttons and keep the button active with page active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    link_to link_text, link_path, :class => class_name
+  end
 end
