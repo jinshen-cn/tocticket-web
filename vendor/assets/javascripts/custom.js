97a9891585920c718215991886b704b2930999c5
Juan Lebrijo <juan@lebrijo.com>
2012-12-10 21:31:33 +0100

    #174 Avoid show select without options on landing page for small screens

--- vendor/assets/javascripts/custom.js
+++ vendor/assets/javascripts/custom.js
@@ -118,37 +118,40 @@ $(function () {
 });
 //Dynamically adds drop down for smaller screens.
 $(function ($) {
-    $("#main-nav").append("<select/>");
-    $("<option />", {
-        "selected": "selected",
-        "value": "",
-        "text": "Please choose page"
-    }).appendTo("#main-nav select");
-    //new dropdown menu
-    $("#main-nav a").each(function () {
-        var el = $(this);
-        var perfix = '';
-        switch (el.parents().length) {
-            case (11):
-                perfix = '-';
-                break;
-            case (13):
-                perfix = '--';
-                break;
-            default:
-                perfix = '';
-                break;
-
-        }
-        $("<option />", {
-            "value": el.attr("href"),
-            "text": perfix + el.text()
-        }).appendTo("#main-nav select");
-    });
-
-    $('#main-nav select').change(function () {
-
-        window.location.href = this.value;
-
-    });
+	if ($("#main-nav ul").children().length) {
+	    $("#main-nav").append("<select/>");
+	    $("<option />", {
+	        "selected": "selected",
+	        "value": "",
+	        "text": "Please choose page"
+	    }).appendTo("#main-nav select");
+	    
+	    //new dropdown menu
+	    $("#main-nav a").each(function () {
+	        var el = $(this);
+	        var perfix = '';
+	        switch (el.parents().length) {
+	            case (11):
+	                perfix = '-';
+	                break;
+	            case (13):
+	                perfix = '--';
+	                break;
+	            default:
+	                perfix = '';
+	                break;
+	
+	        }
+	        $("<option />", {
+	            "value": el.attr("href"),
+	            "text": perfix + el.text()
+	        }).appendTo("#main-nav select");
+	    });
+	
+	    $('#main-nav select').change(function () {
+	
+	        window.location.href = this.value;
+	
+	    });
+    }
 });
