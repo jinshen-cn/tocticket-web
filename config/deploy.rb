db79643be991ccbdb4ecfb8f2a7551cc6a9310f8
Juan Lebrijo <juan@lebrijo.com>
2013-02-07 12:08:47 +0100

    #187 DDBB, bundle and assets tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,11 +9,18 @@ set :scm, :git
 set :repository,  "git@labs.lebrijo.com:gogetix-web.git"
 server "gogetix.com", :app, :web, :db, :primary => true
 set(:deploy_to) {"/var/www/#{application}/#{stage}"} # This makes lazy the load
+set(:rake_command) {"cd #{current_path} && bundle exec rake RAILS_ENV=#{stage}"}
+
+# Thin
+set :thin_config_path, "/etc/thin"
+set(:thin_config_file) {"#{thin_config_path}/#{application}-#{stage}.yml"}
 
 ## rvm
 set :rvm_ruby_string, 'ruby-1.9.3-p327@gogetix-web'
 set :rvm_type, :system
 require "rvm/capistrano"
+
+require "bundler/capistrano"
 
 set :user, "root"
 set :user_sudo, false
@@ -22,23 +29,36 @@ set :user_sudo, false
 namespace :thin do
   desc "Sets up Thin server environments"
   task :setup, :roles => :app do
-    invoke_command "thin config -C /etc/thin/#{application}-#{stage}.yml -c #{deploy_to}/current -e #{stage} --servers #{thin_servers} --port #{thin_port}"
+    invoke_command "thin config -C #{thin_config_file} -c #{current_path} -e #{stage} --servers #{thin_servers} --port #{thin_port}"
   end
 end
 
-
+namespace :db do
+  desc "Set up the database: create, migrate and seed"
+  task :setup, :roles => :db do
+    run "#{rake_command} db:drop"
+    run "#{rake_command} db:create"
+    run "#{rake_command} db:migrate"
+    run "#{rake_command} db:seed"
+  end
+end
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
 
 namespace :deploy do
-  desc "Custom AceMoney deployment: stop."
+  desc "Precompile all application assets"
+  task :precompile_assets, :roles => :app do
+    run "#{rake_command} assets:clean"
+    run "#{rake_command} assets:precompile"
+  end
+  desc "Stop server"
   task :stop, :roles => :app do
 
     invoke_command "service thin stop"
   end
 
-  desc "Custom AceMoney deployment: start."
+  desc "Start server"
   task :start, :roles => :app do
 
     invoke_command "service thin start"
@@ -46,7 +66,7 @@ namespace :deploy do
 
   # Need to define this restart ALSO as 'cap deploy' uses it
   # (Gautam) I dont know how to call tasks within tasks.
-  desc "Custom AceMoney deployment: restart."
+  desc "Restart server"
   task :restart, :roles => :app do
 
     invoke_command "service thin stop"
