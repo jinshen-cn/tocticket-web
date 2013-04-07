35d1e09d7e00f8df2406c4fc68f01b9dd566a0f5
Juan Lebrijo <juan@lebrijo.com>
2013-04-07 23:01:18 +0200

    #230 Configuring staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -70,13 +70,13 @@ GogetixWeb::Application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+      address: "mail.tocticket.com",
+      port: 26,
+      domain: "tocticket.com",
+      authentication: "plain",
+      enable_starttls_auto: false,
+      user_name: "admin+tocticket.com",
+      password: "tocticket123"
   }
 
 
