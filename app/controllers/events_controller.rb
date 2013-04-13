5c882b51b01be1614fe0735439695a73ff747a44
Juan Lebrijo <juan@lebrijo.com>
2013-04-13 22:53:29 +0200

    #165 Clean JSON responses from controllers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,25 +3,13 @@ class EventsController < ApplicationController
   before_filter :is_event_organizer, :only => [:edit, :update, :destroy]
   
   # GET /events
-  # GET /events.json
   def index
     @events = Event.where("organizer_id=?",current_user.id)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @events }
-    end
   end
 
   # GET /events/1
-  # GET /events/1.json
   def show
     @event = Event.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @event }
-    end
   end
 
   def to_uri
@@ -34,15 +22,9 @@ class EventsController < ApplicationController
   end
 
   # GET /events/new
-  # GET /events/new.json
   def new
     @event = Event.new
     @event.paypal_account = current_user.email
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @event }
-    end
   end
 
   # GET /events/1/edit
@@ -50,46 +32,29 @@ class EventsController < ApplicationController
   end
 
   # POST /events
-  # POST /events.json
   def create
     @event = Event.new(params[:event])
     @event.organizer = current_user
 
-    respond_to do |format|
-      if @event.save
-        format.html #{ redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render json: @event, status: :created, location: @event }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+    unless @event.savee
+      render action: "new"
     end
   end
 
   # PUT /events/1
-  # PUT /events/1.json
   def update
-
-    respond_to do |format|
       if @event.update_attributes(params[:event])
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { head :no_content }
+        redirect_to @event, notice: 'Event was successfully updated.'
       else
-        format.html { render action: "edit" }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
+        render action: "edit"
       end
-    end
   end
 
   # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event.destroy
 
-    respond_to do |format|
-      format.html { redirect_to events_url }
-      format.json { head :no_content }
-    end
+    redirect_to events_url
   end
   
   private
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,47 +2,24 @@ class TicketsController < ApplicationController
   before_filter :authenticate_user!, :except => [:new, :create, :secure_ticket]
   before_filter :get_event, :except => :my_tickets
   # GET /my_tickets
-  # GET /my_tickets.json
   def my_tickets
     @tickets = Ticket.where("user_id=?",current_user.id)
-
-    respond_to do |format|
-      format.html # my_tickets.html.erb
-      format.json { render json: @tickets }
-    end
   end
 
   # GET /ticket/:random_key
-  # GET /ticket/:random_key.json
   def secure_ticket
     @ticket = Ticket.find_by_id_and_random_key(params[:ticket_id], params[:random_key])
-
-    respond_to do |format|
-      format.html { render 'show' } # show.html.erb
-      format.json { render json: @ticket }
-    end
+    render 'show'
   end
 
   # GET /events/:event_id/tickets/1
-  # GET /events/:event_id/tickets/1.json
   def show
     @ticket = Ticket.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @ticket }
-    end
   end
 
   # GET /events/:event_id/tickets/new
-  # GET /events/:event_id/tickets/new.json
   def new
     @ticket = Ticket.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @ticket }
-    end
   end
 
   # GET /events/:event_id/tickets/1/edit
@@ -51,7 +28,6 @@ class TicketsController < ApplicationController
   end
 
   # POST /events/:event_id/tickets
-  # POST /events/:event_id/tickets.json
   def create
     @ticket = Ticket.new(params[:ticket])
     @ticket.event = @event
@@ -71,32 +47,22 @@ class TicketsController < ApplicationController
   end
 
   # PUT /events/:event_id/tickets/1
-  # PUT /events/:event_id/tickets/1.json
   def update
     @ticket = Ticket.find(params[:id])
-
-    respond_to do |format|
       if @ticket.update_attributes(params[:ticket])
-        format.html { redirect_to event_ticket_url(@event, @ticket), notice: 'Ticket was successfully updated.' }
-        format.json { head :no_content }
+        redirect_to event_ticket_url(@event, @ticket), notice: 'Ticket was successfully updated.'
       else
-        format.html { render action: "edit" }
-        format.json { render json: @ticket.errors, status: :unprocessable_entity }
+        render action: "edit"
       end
-    end
   end
 
   # DELETE /events/:event_id/tickets/1
-  # DELETE /events/:event_id/tickets/1.json
   def destroy
     @ticket = Ticket.find(params[:id])
     @ticket.destroy
-
-    respond_to do |format|
-      format.html { redirect_to event_url(@event) }
-      format.json { head :no_content }
-    end
+    redirect_to event_url(@event)
   end
+
   private
   def get_event
     @event = Event.find(params[:event_id])
