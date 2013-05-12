37b7594c84b89ad36fcadeb2c47f3f7ea88468fb
Juan Lebrijo <juan@lebrijo.com>
2013-05-12 21:38:51 +0200

    #239 translating

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
-      render("custom_fields/#{association.to_s.singularize}_fields", f: builder)
+      render("custom_fields/line_form", f: builder)
     end
     link_to(name, '#', class: "btn btn-success add_fields", data: {id: id, fields: fields.gsub("\n", "")})
   end
--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -1,4 +1,10 @@
 class CustomField < ActiveRecord::Base
   belongs_to :event
   attr_accessible :field_type, :name, :required
+
+  validates_presence_of :field_type, :name
+
+  FIELD_TYPES = [[I18n.t('events.custom_fields.text_field'), 'text_field'],
+                 [I18n.t('events.custom_fields.check_box'), 'check_box'],
+                 [I18n.t('events.custom_fields.image'), 'image']]
 end
