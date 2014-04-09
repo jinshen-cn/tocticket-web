264cd026110318b6f43001d4acb30a2c20bca801
Juan Lebrijo <juan@lebrijo.com>
2014-04-09 17:11:37 +0200

    DB name depends on env

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,4 +5,4 @@ set :repository,  "git@labs.lebrijo.com:#{application}.git"
 
 set :database_username, 'pg_user'
 set :database_password, '12@tr%'
-set :database_name, 'tocticket_staging'
+set :database_name, 'tocticket'
